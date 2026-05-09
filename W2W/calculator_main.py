9573a31511116cc6537c294a4223f6b34298d258
Chen-Zhichao <zhichaochen@ucla.edu>
2026-05-08 17:03:31 -0700

    Add warpage yield simulation for D2W

--- W2W/calculator_main.py
+++ W2W/calculator_main.py
@@ -80,7 +80,7 @@ def main():
     )
     print(f"Modeled yield: {stack_assembly_yield:.6f}")
     print(f"Yield modeling finished in {time.time() - start_time:.2f} s")
-    print_run_separator(f"W2W risk map generation finished for {args.ds_name}")
+    print_run_separator(f"W2W yield modeling finished for {args.ds_name}")
 
 if __name__ == "__main__":
     main()
